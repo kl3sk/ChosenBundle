0eb4f664396e93ade692e98a7a73c90a4bc5fc2e
klesk <klesk44@gmail.com>
2015-04-12 19:44:57 +0200

    Added host

--- Tests/Controller/DefaultControllerTest.php
+++ Tests/Controller/DefaultControllerTest.php
@@ -6,12 +6,31 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+
+	private function getHostUrl($env, $path = '')
+	{
+	    return self::$kernel->getContainer()->getParameter('dealon.test.hostname.' . $env).$path;
+	}
+
+    public function testDevIndex()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/hello/Fabien');
+        $crawler = $client->request('GET', $this->getHostUrl('dev', '/chosen/hello/Klesk'));
 
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        var_dump($crawler->filter('html:contains("Klesk")'));
+
+        $this->assertTrue($crawler->filter('html:contains("Klesk")')->count() > 0);
+    }
+
+    public function testProdIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', $this->getHostUrl('prod', '/chosen/hello/Klesk'));
+
+        var_dump($crawler->filter('html:contains("Klesk")'));
+
+        $this->assertTrue($crawler->filter('html:contains("Klesk")')->count() > 0);
     }
 }
